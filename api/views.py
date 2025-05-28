7f7018d9ca28398905bca4fb8e56104397f394d3
mishatunikov <michael.tunikov@gmail.com>
2025-05-28 14:06:59 +0300

    feat: create ItemAPIView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from items.models import Item
+
+
+class ItemAPIView(APIView):
+    """
+    Returns an HTML payment form with item details rendered f
+    rom 'payment_form.html'.
+    """
+
+    renderer_classes = [TemplateHTMLRenderer,]
+    template_name = 'payment_form.html'
+
+    def get(self, request, pk):
+        instance = get_object_or_404(Item, pk=pk)
+        return Response({'instance': instance})
