c1cab094118de7ae04c8a3a102502d8f60177eb3
mishatunikov <michael.tunikov@gmail.com>
2025-05-29 16:28:08 +0300

    feat: create Order and Tax model, create abstract models

--- items/models.py
+++ items/models.py
@@ -1,13 +1,35 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from items import consts
 
 
-class Item(models.Model):
+class BaseName(models.Model):
+    """Abstract model with name field."""
+
     name = models.CharField(
         max_length=consts.MAX_NAME_LENGTH, unique=True, verbose_name='название'
     )
+
+    class Meta:
+        abstract = True
+        ordering = ('name',)
+
+
+class TaxDiscountBase(BaseName):
+    """Abstract model with stripe_id and name fields."""
+
+    stripe_id = models.CharField(
+        max_length=consts.STRIPE_ID_MAX_LENGTH, blank=True, null=True,
+    )
+
+    class Meta(BaseName.Meta):
+        abstract = True
+
+
+class Item(BaseName):
+    """Item model."""
+
     description = models.TextField(
         max_length=consts.MAX_TEXT_LENGTH, verbose_name='описание'
     )
@@ -20,16 +42,17 @@ class Item(models.Model):
         verbose_name='цена',
     )
 
-    class Meta:
+    class Meta(BaseName.Meta):
         verbose_name = 'товар'
         verbose_name_plural = 'Товары'
-        ordering = ('name',)
 
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
+    """Order model."""
+
     items = models.ManyToManyField(
         Item, related_name='orders', verbose_name='товары'
     )
@@ -44,3 +67,38 @@ class Order(models.Model):
 
     def __str__(self):
         return f'Заказ №{self.pk}'
+
+
+class Discount(TaxDiscountBase):
+    """Discount model"""
+
+    percent_off = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(consts.MAX_DISCOUNT_PERCENT),
+            MaxValueValidator(consts.MIN_DISCOUNT_PERCENT),
+        ],
+        verbose_name='процент скидки',
+    )
+
+    class Meta(TaxDiscountBase.Meta):
+        verbose_name = 'скидка'
+        verbose_name_plural = 'скидки'
+
+    def __str__(self):
+        return f'{self.name} ({self.percent_off}%'
+
+
+class Tax(TaxDiscountBase):
+    """Tax model."""
+
+    percentage = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(consts.MIN_TAX_PERCENT)],
+        verbose_name='процент налога',
+    )
+
+    class Meta(TaxDiscountBase.Meta):
+        verbose_name = 'налог'
+        verbose_name_plural = 'налоги'
+
+    def __str__(self):
+        return f'{self.name} ({self.percentage}%)'
