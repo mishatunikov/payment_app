840f0be3deb4411eb377197d67c1b48a6b97c99e
mishatunikov <michael.tunikov@gmail.com>
2025-05-29 11:26:47 +0300

    feat: register buy/{}/ endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 
-from api.views import ItemAPIView
+from api.views import ItemAPIView, StripeSessionAPIView
 
 urlpatterns = [
     path('v1/item/<int:pk>/', ItemAPIView.as_view()),
+    path('v1/buy/<int:pk>/', StripeSessionAPIView.as_view()),
 ]
