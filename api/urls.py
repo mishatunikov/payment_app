4d8b1de57f918b43aa738db7d055509e41c2835a
mishatunikov <michael.tunikov@gmail.com>
2025-05-30 16:17:25 +0300

    feat: register order endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 
-from api.views import ItemAPIView, StripeSessionAPIView
+from api.views import BuyItemView, ItemAPIView, MakeOrderView, OrderApiView
 
 urlpatterns = [
     path('v1/item/<int:pk>/', ItemAPIView.as_view()),
-    path('v1/buy/<int:pk>/', StripeSessionAPIView.as_view()),
+    path('v1/buy/<int:pk>/', BuyItemView.as_view()),
+    path('v1/order/<int:pk>/', OrderApiView.as_view()),
+    path('v1/order/<int:pk>/payment/', MakeOrderView.as_view()),
 ]
