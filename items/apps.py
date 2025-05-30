c1ec5cdb2b90bd8ede427aafc307a841a38e2a38
mishatunikov <michael.tunikov@gmail.com>
2025-05-30 16:17:45 +0300

    feat: add signal in app

--- items/apps.py
+++ items/apps.py
@@ -5,3 +5,6 @@ class ItemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'items'
     verbose_name = 'товары'
+
+    def ready(self):
+        import items.signals
